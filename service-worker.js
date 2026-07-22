50a60fb5364a160c327c71ca106d24a9a59cad0d
DDRNFinch <dfinch1984@hotmail.co.uk>
2026-07-22 23:08:05 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const VERSION='3.3';
-const CACHE=`applus-${VERSION}-rewards-tile`;
+const VERSION='3.4';
+const CACHE=`applus-${VERSION}-xp-system`;
 const CORE=['./','index.html','app.css','app.js','courses.js','academy-questions.js','manifest.json','version.json','logo.png','icon-192.png','icon-512.png'];
 
 self.addEventListener('install',event=>{
